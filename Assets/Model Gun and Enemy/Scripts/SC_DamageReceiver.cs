6a61270d63494547220b14fc41048d1ceafe5e64
minnemint <84864474+minnemint@users.noreply.github.com>
2023-06-22 22:30:49 +0700

    2262023

---
+++ Assets/Model Gun and Enemy/Scripts/SC_DamageReceiver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SC_DamageReceiver : MonoBehaviour, IEntity
+{
+    //This script will keep track of player HP
+    public float playerHP = 100;
+    public SC_CharacterController playerController;
+    public SC_WeaponManager weaponManager;
+
+    public void ApplyDamage(float points)
+    {
+        playerHP -= points;
+
+        if(playerHP <= 0)
+        {
+            //Player is dead
+            playerController.canMove = false;
+            playerHP = 0;
+        }
+    }
+}
---
+++ Assets/Model Gun and Enemy/Scripts/SC_WeaponManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class SC_WeaponManager : MonoBehaviour
+{
+    public Camera playerCamera;
+    public SC_Weapon primaryWeapon;
+    public SC_Weapon secondaryWeapon;
+
+    [HideInInspector]
+    public SC_Weapon selectedWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //At the start we enable the primary weapon and disable the secondary
+        primaryWeapon.ActivateWeapon(true);
+        secondaryWeapon.ActivateWeapon(false);
+        selectedWeapon = primaryWeapon;
+        primaryWeapon.manager = this;
+        secondaryWeapon.manager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Select secondary weapon when pressing 1
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            primaryWeapon.ActivateWeapon(false);
+            secondaryWeapon.ActivateWeapon(true);
+            selectedWeapon = secondaryWeapon;
+        }
+
+        //Select primary weapon when pressing 2
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            primaryWeapon.ActivateWeapon(true);
+            secondaryWeapon.ActivateWeapon(false);
+            selectedWeapon = primaryWeapon;
+        }
+    }
+}
